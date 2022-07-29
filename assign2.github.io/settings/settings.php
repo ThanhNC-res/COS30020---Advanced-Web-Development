846a2e9aec87fb616bd17cef8a43df4618e5bd8d
Ngo Cong Thanh <110225420+ThanhNC-res@users.noreply.github.com>
2022-07-29 21:28:43 +0700

    Delete assign2.github.io directory

--- assign2.github.io/settings/settings.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$host = "feenix-mariadb.swin.edu.au"; 
-$user = "s103433609"; // your user name 
-$pswd = "281002"; // your password d(date of birth – ddmmyy) 
-$dbnm = "s103433609_db"; // your database 
-?> 
- 
